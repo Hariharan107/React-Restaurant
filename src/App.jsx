5dcdda0be7844bf7661045922eee1f8a2948abff
Hariharan <hariharen107@gmail.com>
2023-07-05 22:35:22 +0530

    Created routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
-import { useState } from "react";
-function App() {
-  const [count, setCount] = useState(0);
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "/order/new",
+    element: <CreateOrder />,
+  },
+  {
+    path: "/order/:id",
+    element: <Order />,
+  }
 
-  return (
-    <>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+]);
+function App() {
+  return <RouterProvider router={router} />;
 }
 
 export default App;
