d65ecbee2b31ccf1a1ac28f09d12816cc857ef4a
Hariharan <hariharen107@gmail.com>
2023-07-06 12:23:07 -0700

    Added AppLayout route

---
+++ src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+      <Link to='/'>React Restaurant</Link>
+      <p>HARI</p>
+    </header>
+  );
+};
+
+export default Header;
