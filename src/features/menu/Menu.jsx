972d8dd991811b999f1ca7fe7c07b65df3c93afd
Hariharan <hariharen107@gmail.com>
2023-07-06 14:35:43 -0700

    Added loader and error states

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,5 +1,19 @@
-function Menu() {
-  return <h1>Menu</h1>;
-}
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "./MenuItem";
 
+const Menu = () => {
+  const menu = useLoaderData();
+  return (
+    <ul>
+      {menu.map((pizza) => (
+        <MenuItem key={pizza.id} {...pizza} />
+      ))}
+    </ul>
+  );
+};
+export const loader = async () => {
+  const menu = await getMenu();
+  return menu;
+};
 export default Menu;
