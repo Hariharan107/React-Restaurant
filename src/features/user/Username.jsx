a2f2278ff828d9f16a39c4d55fb38e786b757917
Hariharan <hariharen107@gmail.com>
2023-07-08 16:58:17 +0530

    Redux Setup

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,9 @@
+import { useSelector } from 'react-redux';
+
 function Username() {
-  return <div className="hidden text-sm font-semibold md:block">Hari</div>;
+  const userName = useSelector((state) => state.user.userName);
+  const user = userName ? `Welcome ${userName}` : 'Welcome Guest';
+  return <div className='hidden text-sm font-semibold md:block'>{user}</div>;
 }
 
 export default Username;
