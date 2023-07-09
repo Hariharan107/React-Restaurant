a415e9f7413c3a4b042e4aa063b96b0cdf18cec0
Hariharan <hariharen107@gmail.com>
2023-07-09 19:26:24 +0530

    Intgreted thunks with redux

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -3,9 +3,10 @@ import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
 import store from '../../store/store';
 import Button from '../../ui/Button';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { clearCart, getCart, getTotalCartPrice } from '../cart/cartSlice';
 import EmptyCart from '../cart/EmptyCart';
+import { fetchAddressthunk } from '../user/userSlice';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -23,12 +24,14 @@ function CreateOrder() {
   const cartPrice = useSelector(getTotalCartPrice);
   const priorityPrice = withPriority ? cartPrice * 0.2 : 0;
   const finalCartPrice = cartPrice + priorityPrice;
-
+  const dispatch = useDispatch();
   if (!cart.length) return <EmptyCart />;
   return (
     <div className='px-4 py-6'>
       <h2 className='mb-8 text-xl font-semibold'>Ready to order? Let's go!</h2>
-
+      <button onClick={() => dispatch(fetchAddressthunk())}>
+        get position
+      </button>
       {/* <Form method="POST" action="/order/new"> */}
       <Form method='POST'>
         <div className='mb-5 flex flex-col gap-2 sm:flex-row sm:items-center'>
